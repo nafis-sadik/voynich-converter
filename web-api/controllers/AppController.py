ba9be978a43b4535c771f311e9c380b56bcad827
nafis-sadik <nafis_sadik@outlook.com>
2022-05-13 18:50:28 +0600

    All done, needs testing

--- web-api/controllers/AppController.py
+++ web-api/controllers/AppController.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter
-import fastapi
-from starlette.responses import JSONResponse
-from Entities import create_db
+#import fastapi
+#from starlette.responses import JSONResponse
 
-from sys import path as pylib
-import os
-pylib += [os.path.abspath(r'../Repositories')]
+#from sys import path as pylib
+#import os
+#pylib += [os.path.abspath(r'../Repositories')]
+
+#from Entities import create_db
 
 app = APIRouter(prefix='/app')
 
-@app.get('/CreateDatabase')
-def create_database():
-    create_db()
-    response='success'
-    return JSONResponse(response)
+#@app.get('/CreateDatabase')
+#def create_database():
+#    create_db()
+#    response='success'
+#    return JSONResponse(response)
--- web-api/controllers/DownloaderController.py
+++ web-api/controllers/DownloaderController.py
@@ -1,6 +1,8 @@
-from types import resolve_bases
 from fastapi import APIRouter
+from fastapi.responses import FileResponse
+from pydantic.types import confloat
 import wget
+import threading
 import moviepy.editor as mp
 from datetime import datetime
 
@@ -9,34 +11,45 @@ import os
 pylib += [os.path.abspath(r'../Repositories')]
 
 from models import ConvertionConfigModel
-from Entities import SysConfig
-from RepositoryBase import RepositoryBase
-from IRepositoryBase import IRepositoryBase
 
 downloader = APIRouter(prefix='/dw')
 
+def delayed_file_remove(path: str, delay_in_mins: int):
+    time.sleep(delay_in_mins * 10)
+    os.remove(path)
 
-def convert_to_mp3(file_path: str):
-    print('Shoot')
+
+def convert_to_mp3(file_path: str, start_time: int, end_time: int, destination_file_name: str):
     clip = mp.VideoFileClip(file_path)
-    audio_file_path: str = file_path.split('.')[0] + file_path.split('.')[1] + '.mp3'
-    result = clip.audio.write_audiofile(audio_file_path)
-    return result
+    if end_time == 0:
+        end_time = clip.duration
+    clip = clip.subclip(start_time, end_time)
+    audio_file_path: str = './' + file_path.split('.')[0] + file_path.split('.')[1] + '.mp3'
+    clip.audio.write_audiofile(audio_file_path)
+    clip.close()
+    return audio_file_path
 
 
 def download_video(url: str):
     downloaded_file = wget.download(url, out='./static')
+    source_file = source_file_path(downloaded_file)
+    os.rename(downloaded_file, source_file)
+    return source_file
+
+def source_file_path(downloaded_file):
     file_path: str = downloaded_file + '_' + str(datetime.timestamp(datetime.now())) + '.mp4'
-    os.rename(downloaded_file, file_path)
     return file_path
 
 
-@downloader.post('/')
+@downloader.post('/mp3')
 def download_mp3(config: ConvertionConfigModel):
     source_file = download_video(config.url)
     if os.path.exists(source_file):
-        response = convert_to_mp3(source_file)
+        audio_file_path = convert_to_mp3(source_file, config.start_time, config.end_time, config.file_name)
         os.remove(source_file)
+        delete_audio_thread = threading.Thread(target=delayed_file_remove, args=(audio_file_path, 1))
+        delete_audio_thread.start()
+        return FileResponse(audio_file_path)
     else:
         response = None
     return response
